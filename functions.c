ab2883eeedb4fa329e4098d76a7bcb7c412aca45
daviddlhz <vidda1959@gmail.com>
2020-04-13 11:24:19 -0500

    Add function strstr to functions.c and the prototype is add in the lb_shell.h

--- functions.c
+++ functions.c
@@ -57,3 +57,30 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (0);
 }
+/**
+ * _strstr - check the code for Holberton School students.
+ * @haystack: is the parameter.
+ * @needle: is other parameter.
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *beginning = haystack;
+		char *pattern = needle;
+
+		while (*pattern == *haystack && *pattern != '\0'
+		       && *haystack != '\0')
+		{
+			haystack++;
+			pattern++;
+		}
+		if (*pattern == '\0')
+		{
+			return (beginning);
+		}
+		haystack = beginning + 1;
+	}
+	return (NULL);
+}
--- lb_shell.h
+++ lb_shell.h
@@ -9,10 +9,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/*Macros*/
 #define DELIMITERS " \t\r\n\a :"
 #define NEWLINE write(STDOUT_FILENO, "\n", 1)
-
-extern char **environ;
+#define SEARCH_PATH "PATH"
+#define DELIMITER_PATH "=:"
+#define MAX_SIZE 500
 
 /*Prototypes*/
 void perror(const char *str);
@@ -22,5 +24,11 @@ char *_strdup(char *);
 void split_buffer(char *);
 void exec_program(char **);
 int _strcmp(char *, char *);
+//char ** path_get(void);
+void _getenv(void);
+char *_strstr(char *, char *);
+
+/*Environment*/
+extern char **environ;
 
 #endif
