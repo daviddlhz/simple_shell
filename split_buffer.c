ac6c610691b48a81cf2ed09d5158e19ef55fc8d8
daviddlhz <vidda1959@gmail.com>
2020-04-14 10:06:24 -0500

    Add changes of function split buffer

--- split_buffer.c
+++ split_buffer.c
@@ -4,11 +4,15 @@
  * @buffer: is a pointer.
  * Return: Split words.
  */
-void split_buffer(char *buffer)
+void split_buffer(char *buffer, char *path)
 {
-	int i = 0;
-	char *commands[100];
+	int i = 0, j = 0;
+	char *current_path = NULL;
+	char *commands[MAX_SIZE];
+	char *path_cut[MAX_SIZE];
 
+	current_path = _strdup(path);
+	printf("\n current PATH: %s\n", current_path);
 	buffer = strtok(buffer, DELIMITERS);
 
 	while (buffer != NULL)
@@ -17,7 +21,21 @@ void split_buffer(char *buffer)
 		buffer = strtok(NULL, DELIMITERS);
 		i++;
 	}
-	commands[i] = NULL;
+	commands[i] = '\0';
 	free(buffer);
+
+		j = 0;
+		path = strtok(current_path, DELIMITER_PATH);
+
+		while (current_path != NULL)
+		{
+			path_cut[j] = current_path;
+			current_path = strtok(NULL, DELIMITER_PATH);
+			printf("PATH CUT : %s \n", path_cut[j]);
+			j++;
+		}
+		path_cut[j] = '\0';
+
 	exec_program(commands);
+	concat_path(path_cut);
 }
