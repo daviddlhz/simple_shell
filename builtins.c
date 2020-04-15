ce7eac9732a6a250bf9f27c7d06ccebc411dc7d3
daviddlhz <vidda1959@gmail.com>
2020-04-15 17:19:38 -0500

    Fix betty in the file builtins.c

--- builtins.c
+++ builtins.c
@@ -1,10 +1,19 @@
 #include "lb_shell.h"
-
+/**
+ * clear: Clear the display of terminal.
+ *
+ * Return: Nothing.
+ */
 void clear(void)
 {
 	write(STDOUT_FILENO, "\033[H\033[J", 6);
 }
 
+/**
+ * s_exit: exit of the terminal.
+ *
+ * Return: Nothing.
+ */
 void s_exit(void)
 {
 	exit(EXIT_SUCCESS);
